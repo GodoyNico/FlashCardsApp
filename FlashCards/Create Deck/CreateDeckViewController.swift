59ebc1d3d1ec698fa38d40fdcb6a3aabd1ad61c6
Julia <juliasilveira.dsouza@gmail.com>
2021-10-26 18:18:21 -0300

    TK16 in progress reformulada

--- FlashCards/Create Deck/CreateDeckViewController.swift
+++ FlashCards/Create Deck/CreateDeckViewController.swift
@@ -14,29 +14,29 @@ class CreateDeckViewController: UIViewController {
     
     var decks: [Deck] = []
     
-    @IBOutlet weak var createDeck: UIBarButtonItem!
+    @IBOutlet weak var deckDone: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var newDeckTextfield: UITextField!
+    @IBOutlet weak var numberOfCardsLabel: UILabel!
+    @IBOutlet weak var addCardButton: UIButton!
     
     @IBAction func createDeck(_ sender: Any) {
-        
-//        // Get textfield for the alert
-//        let textField = alert.textFields!.first
-//
-//        // Create a Deck Object
-//        let newDeck = Deck(context: self.context)
-//        newDeck.title = textField?.text
-//        newDeck.id = UUID()
-//        newDeck.created_date = Date.now
-//
-//        // Save the Data
-//        do {
-//            try self.context.save()
-//        } catch { }
-//
-//        // Re-Fetch the Data
-//        self.fetchDecks()
+
+        // Create a Deck Object
+        let newDeck = Deck(context: self.context)
+        newDeck.title = newDeckTextfield.text
+        newDeck.id = UUID()
+        newDeck.created_date = Date.now
+
+        // Save the Data
+        do {
+            try self.context.save()
+        } catch { }
+
+        // Re-Fetch the Data
+        self.fetchDecks()
         
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
