2e0564fd0c1d8ceecd09529c8245e1852f94be14
Igor Vicente <Igor.Vicente@edu.pucrs.br>
2021-11-05 03:11:16 -0400

    layout singledeck circle pregress : done

--- FlashCards/CircularProgressView.swift
+++ FlashCards/CircularProgressView.swift
@@ -66,6 +66,7 @@ class CircularProgressView: UIView {
     func setValue(value: CGFloat) {
         self.backgroundColor = UIColor.clear
         self.layer.cornerRadius = self.frame.size.width/2
+
         let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width/2, y: frame.size.height/2), radius: (frame.size.width - 1.5)/2, startAngle: CGFloat(-0.5 * .pi), endAngle: CGFloat(1.5 * .pi), clockwise: true)
         
         trackLayer.path = circlePath.cgPath
@@ -84,13 +85,13 @@ class CircularProgressView: UIView {
     }
     
     func setProgressWithAnimation(duration: TimeInterval, value: Float) {
-//        let animation = CABasicAnimation(keyPath: "strokeEnd")
-//        animation.duration = duration
-//        animation.fromValue = 0
-//        animation.toValue = value
-//        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = duration
+        animation.fromValue = 0
+        animation.toValue = value
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
         progressLayer.strokeEnd = CGFloat(value)
-        //progressLayer.add(animation, forKey: "animateprogress")
+        progressLayer.add(animation, forKey: "animateprogress")
     }
 
 }
--- FlashCards/Deck/SingleDeckViewController.swift
+++ FlashCards/Deck/SingleDeckViewController.swift
@@ -9,17 +9,33 @@ import UIKit
 
 class SingleDeckViewController: UIViewController {
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     var deck: Deck?
 
     @IBOutlet weak var deckTtitleLabel: UILabel!
     @IBOutlet weak var practiceButton: UIButton!
+    @IBOutlet weak var deckProgressCircleView: CircularProgressView!
+    
     let goToPracticeSegueID: String = "goToPractice"
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        deckTtitleLabel.text = deck?.title
-        practiceButton.layer.cornerRadius = 10
-        practiceButton.backgroundColor = UIColor(named: "gray1")
+        if let deckSelected = self.deck {
+            deckTtitleLabel.text = deckSelected.title
+            practiceButton.layer.cornerRadius = 10
+            practiceButton.backgroundColor = UIColor(named: "gray1")
+        } else {
+            self.deck = Deck(context: self.context)
+            deck?.title = "teste"
+            deckTtitleLabel.text = deck!.title
+            practiceButton.layer.cornerRadius = 10
+            practiceButton.backgroundColor = UIColor(named: "gray1")
+        }
+        deckProgressCircleView.setValue(value: 0.7)
+        deckProgressCircleView.trackColor = UIColor.gray
+        deckProgressCircleView.progressColor = UIColor.black
+        
     }
 
     @IBAction func toPractice(_ sender: Any) {
@@ -30,6 +46,12 @@ class SingleDeckViewController: UIViewController {
         guard let practiceViewController = segue.destination as? PracticeViewController, let deck = sender as? Deck else { return }
 
         practiceViewController.configure(deck: deck)
+    }
+    
+    func configure(deck: Deck) {
+        self.deck = deck
+        
+        
     }
     
 }
