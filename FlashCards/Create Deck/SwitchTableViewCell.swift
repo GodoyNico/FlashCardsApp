4fd62b768aae2406b4265f43f2a62252b826b401
Julia <juliasilveira.dsouza@gmail.com>
2021-11-09 17:35:53 -0300

    Ação do swipe lado que começa a a prática

--- FlashCards/Create Deck/SwitchTableViewCell.swift
+++ FlashCards/Create Deck/SwitchTableViewCell.swift
@@ -9,16 +9,35 @@ import UIKit
 
 class SwitchTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var switchLabel: UILabel!
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var deck: Deck?
     
+    @IBOutlet weak var switchLabel: UILabel!
     @IBOutlet weak var switchButton: UISwitch!
     
-    @IBAction func switchAction(_ sender: Any) {
+    @IBAction func switchAction(_ sender: UISwitch) {
+
+        if switchButton.isOn {
+            self.deck?.isFront = false
+                        
+        } else {
+            self.deck?.isFront = true
+        }
+                
+        // Save the Data
+        do {
+            try self.context.save()
+        } catch { }
+    }
+    
+    func configure(newDeck: Deck?) {
+        self.deck = newDeck
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        switchButton.onTintColor = .blue
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
