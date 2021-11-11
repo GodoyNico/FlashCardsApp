0718d07858a33db6e684e13a18860026622496aa
Nico Godoy <nicogodoyalmeida@outlook.com>
2021-11-11 14:31:22 -0300

    Adjust view of feedback

--- FlashCards/Practice/FeedbackViewController.swift
+++ FlashCards/Practice/FeedbackViewController.swift
@@ -12,6 +12,7 @@ class FeedbackViewController: UIViewController {
     let deckSegueId: String = "goToSingleDeck"
     let myDecksSegueId: String = "goToMyDecks"
     
+    @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var feedbackLabel: UILabel!
     @IBOutlet weak var feedbackText: UILabel!
     @IBOutlet weak var counterLabel: UILabel!
@@ -22,9 +23,7 @@ class FeedbackViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        practiceAgain.layer.cornerRadius = 8
-        goToDecks.layer.cornerRadius = 8
+        configView()
         
         feedbackLabel.text = "Parabéns!"
         feedbackText.text = "Você memorizou"
@@ -44,5 +43,22 @@ class FeedbackViewController: UIViewController {
     
     func deckConfig(data: PracticeFeedback) {
         self.practiceFeedback = data
+    }
+    
+    func configView() {
+        view.backgroundColor = UIColor(designSystem: DesignSystem.AssetsColor.color2Primary)
+        contentView.backgroundColor = UIColor(designSystem: DesignSystem.AssetsColor.color2Primary)
+        
+        feedbackLabel.textColor = UIColor(designSystem: DesignSystem.AssetsColor.color2Secondary)
+        feedbackText.textColor = UIColor(designSystem: DesignSystem.AssetsColor.color2Secondary)
+        counterLabel.textColor = UIColor(designSystem: DesignSystem.AssetsColor.color2Secondary)
+        deckNameLabel.textColor = UIColor(designSystem: DesignSystem.AssetsColor.basicQuaternary)
+        
+        practiceAgain.layer.cornerRadius = 8
+        practiceAgain.layer.backgroundColor = UIColor(designSystem: DesignSystem.AssetsColor.color1Primary)?.cgColor
+        practiceAgain.tintColor = UIColor(designSystem: DesignSystem.AssetsColor.color2Primary)
+        
+        goToDecks.layer.cornerRadius = 8
+        goToDecks.tintColor = UIColor(designSystem: DesignSystem.AssetsColor.basicQuaternary)
     }
 }
