cff9049b9968f2cd3759b77046da449a7496a6c3
Igor Vicente <Igor.Vicente@edu.pucrs.br>
2021-11-16 11:49:23 -0400

    TabBar Custom : falta mudar a imagem do botão central e ver a cor dos textos

--- FlashCards/TabBarController.swift
+++ FlashCards/TabBarController.swift
@@ -5,6 +5,17 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate = self
+        
+        tabBar.items?[0].title = "Decks"
+        tabBar.items?[1].title = "Praticar"
+        tabBar.items?[2].title = "Ajustes"
+
+        tabBar.items?[0].image = UIImage(systemName: selectedIndex == 0 ? "list.bullet.rectangle.fill":"list.bullet.rectangle")
+        tabBar.items?[2].image = UIImage(systemName: selectedIndex == 2 ? "gearshape.fill":"gearshape")
+
+        tabBar.tintColor = UIColor(designSystem: DesignSystem.AssetsColor.tabBarTint)
+        tabBar.barTintColor = UIColor(designSystem: DesignSystem.AssetsColor.tabBar)
+        setupMiddleButton()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -23,15 +34,45 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
             return true
         }
         
+        randomPractice()
+        return false
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if item == tabBar.items?[0] {
+            tabBar.items?[0].image = UIImage(systemName: "list.bullet.rectangle.fill")
+            tabBar.items?[2].image = UIImage(systemName: "gearshape")
+        } else {
+            tabBar.items?[0].image = UIImage(systemName: "list.bullet.rectangle")
+            tabBar.items?[2].image = UIImage(systemName: "gearshape.fill")
+        }
+    }
+    
+    func setupMiddleButton() {
+        let middleButton = UIButton(frame: CGRect(x: (self.view.bounds.width / 2) - 30, y: -30, width: 60, height: 60))
+        
+        middleButton.setBackgroundImage(UIImage(named: "home-icon-bg"), for: .normal)
+        middleButton.layer.shadowColor = UIColor.black.cgColor
+        middleButton.layer.shadowOpacity = 0.1
+        middleButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        
+        self.tabBar.addSubview(middleButton)
+        middleButton.addTarget(self, action: #selector(menuButtonAction), for: .touchUpInside)
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    @objc func menuButtonAction(sender: UIButton) {
+        randomPractice()
+    }
+    
+    func randomPractice() {
         let storyBoard = UIStoryboard(name: "Deck", bundle: .main)
         
         guard let singleDeckController = storyBoard.instantiateInitialViewController()
-        else { return false }
+        else { return }
         
         navigationController?.pushViewController(singleDeckController, animated: true)
-        
-        return false
-        
     }
     
 }
